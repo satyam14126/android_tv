b71e44f10c4ef3b865b8e739f10ba783508157ee
root <root@localhost.localdomain>
2026-08-06 10:13:19 +0530

    chore: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.antigravity.tvbrowser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
