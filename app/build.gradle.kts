8fbb1179c6bb0aed5d2983de084934f9706f0161
root <root@localhost.localdomain>
2026-08-06 11:12:53 +0530

    chore: bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.antigravity.tvbrowser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
